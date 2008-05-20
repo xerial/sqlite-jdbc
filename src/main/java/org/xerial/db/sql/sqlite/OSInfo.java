6fd3fbac8bc1f5dc7789d6e1ba3e58073fda8dbe
leo <leo@ae02f08e-27ec-0310-ae8c-8ba02fe2eafd>
2008-05-20 02:28:11 +0000

    git-svn-id: http://www.xerial.org/svn/project/XerialJ/trunk/sqlite-jdbc@2155 ae02f08e-27ec-0310-ae8c-8ba02fe2eafd

---
+++ src/main/java/org/xerial/db/sql/sqlite/OSInfo.java
@@ -0,0 +1,70 @@
+/*--------------------------------------------------------------------------
+ *  Copyright 2008 Taro L. Saito
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *--------------------------------------------------------------------------*/
+//--------------------------------------
+// sqlite-jdbc Project
+//
+// OSInfo.java
+// Since: May 20, 2008
+//
+// $URL$ 
+// $Author$
+//--------------------------------------
+package org.xerial.db.sql.sqlite;
+
+/**
+ * Provides OS name and architecture name.
+ * 
+ * @author leo
+ * 
+ */
+public class OSInfo
+{
+
+    public static String getOSName()
+    {
+        return translateOSNameToFolderName(System.getProperty("os.name"));
+    }
+
+    public static String getArchName()
+    {
+        return translateArchNameToFolderName(System.getProperty("arch.name"));
+    }
+
+    public static String translateOSNameToFolderName(String osName)
+    {
+        if (osName.contains("Windows"))
+        {
+            return "Windows";
+        }
+        else if (osName.contains("Mac"))
+        {
+            return "Mac";
+        }
+        else if (osName.contains("Linux"))
+        {
+            return "Linux";
+        }
+        else
+        {
+            return osName.replaceAll("\\W", "");
+        }
+    }
+
+    public static String translateArchNameToFolderName(String archName)
+    {
+        return archName.replaceAll("\\W", "");
+    }
+}
