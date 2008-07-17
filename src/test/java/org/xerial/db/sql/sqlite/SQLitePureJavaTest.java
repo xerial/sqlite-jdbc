aa13c2d6c0fa55cc9e749625d2209662df48c283
leo <leo@ae02f08e-27ec-0310-ae8c-8ba02fe2eafd>
2008-07-17 03:23:00 +0000

    SQLiteJDBC 3.6.0
    git-svn-id: http://www.xerial.org/svn/project/XerialJ/trunk/sqlite-jdbc@2334 ae02f08e-27ec-0310-ae8c-8ba02fe2eafd

---
+++ src/test/java/org/xerial/db/sql/sqlite/SQLitePureJavaTest.java
@@ -0,0 +1,100 @@
+//--------------------------------------
+// sqlite-jdbc Project
+//
+// SQLiteNestedTest.java
+// Since: Jul 17, 2008
+//
+// $URL$ 
+// $Author$
+//--------------------------------------
+package org.xerial.db.sql.sqlite;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sqlite.Function;
+import org.xerial.util.log.Logger;
+
+public class SQLitePureJavaTest
+{
+    private static Logger _logger = Logger.getLogger(SQLitePureJavaTest.class);
+
+    private Connection connection = null;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        System.setProperty("sqlite.purejava", "true");
+        connection = null;
+        Class.forName("org.sqlite.JDBC");
+        // create a database connection
+        connection = DriverManager.getConnection("jdbc:sqlite::memory:");
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        if (connection != null)
+            connection.close();
+    }
+
+    @Test
+    public void query() throws ClassNotFoundException
+    {
+        _logger.debug(String.format("running in %s mode", SQLiteJDBCLoader.isNativeMode() ? "native" : "nested"));
+
+        try
+        {
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30); // set timeout to 30 sec.
+
+            statement.executeUpdate("create table person ( id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+
+            ResultSet rs = statement.executeQuery("select * from person order by id");
+            while (rs.next())
+            {
+                // read the result set
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+            }
+        }
+        catch (SQLException e)
+        {
+            // if e.getMessage() is "out of memory", it probably means no
+            // database file is found
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void function() throws SQLException
+    {
+        Function.create(connection, "total", new Function() {
+            @Override
+            protected void xFunc() throws SQLException
+            {
+                int sum = 0;
+                for (int i = 0; i < args(); i++)
+                    sum += value_int(i);
+                result(sum);
+            }
+        });
+
+        ResultSet rs = connection.createStatement().executeQuery("select total(1, 2, 3, 4, 5)");
+        assertTrue(rs.next());
+        assertEquals(rs.getInt(1), 1 + 2 + 3 + 4 + 5);
+    }
+
+}
