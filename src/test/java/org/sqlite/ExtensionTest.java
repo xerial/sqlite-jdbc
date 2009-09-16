c5022da5c4b4b3d129f593611d9596ec8d42bf86
leo <leo@ae02f08e-27ec-0310-ae8c-8ba02fe2eafd>
2009-09-16 02:27:03 +0000

    git-svn-id: http://www.xerial.org/svn/project/XerialJ/trunk/sqlite-jdbc@3589 ae02f08e-27ec-0310-ae8c-8ba02fe2eafd

---
+++ src/test/java/org/sqlite/ExtensionTest.java
@@ -0,0 +1,80 @@
+package org.sqlite;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ExtensionTest {
+
+    @BeforeClass
+    public static void forName() throws Exception {
+        Class.forName("org.sqlite.JDBC");
+    }
+
+    Connection conn;
+    Statement stat;
+
+    @Before
+    public void setUp() throws Exception {
+        conn = DriverManager.getConnection("jdbc:sqlite:");
+        stat = conn.createStatement();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (stat != null)
+            stat.close();
+        if (conn != null)
+            conn.close();
+
+    }
+
+    @Test
+    public void extFTS3() throws Exception {
+        if (!SQLiteJDBCLoader.isNativeMode())
+            return;
+
+        stat.execute("create virtual table recipe using fts3(name, ingredients)");
+        stat
+                .execute("insert into recipe (name, ingredients) values('broccoli stew', 'broccoli peppers cheese tomatoes')");
+        stat
+                .execute("insert into recipe (name, ingredients) values('pumpkin stew', 'pumpkin onions garlic celery')");
+
+        ResultSet rs = stat
+                .executeQuery("select rowid, name, ingredients from recipe where ingredients match 'onions'");
+        assertTrue(rs.next());
+        assertEquals("pumpkin stew", rs.getString(2));
+
+    }
+
+    @Test
+    public void extFunctions() throws Exception {
+        if (!SQLiteJDBCLoader.isNativeMode())
+            return;
+
+        {
+            ResultSet rs = stat.executeQuery("select cos(radians(45))");
+            assertTrue(rs.next());
+            assertEquals(0.707106781186548, rs.getDouble(1), 0.000000000000001);
+            rs.close();
+        }
+
+        {
+            ResultSet rs = stat.executeQuery("select reverse(\"ACGT\")");
+            assertTrue(rs.next());
+            assertEquals("TGCA", rs.getString(1));
+            rs.close();
+        }
+
+    }
+
+}
