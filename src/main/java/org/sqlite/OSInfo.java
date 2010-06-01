510773196271c76643795d19dc74b3e94e278830
Taro L. Saito <leo@xerial.org>
2010-06-01 10:36:09 +0900

    add command for retrieving os.name and os.arch

--- src/main/java/org/sqlite/OSInfo.java
+++ src/main/java/org/sqlite/OSInfo.java
@@ -32,48 +32,49 @@ package org.sqlite;
  */
 public class OSInfo
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        if (args.length >= 1) {
+            if ("--os".equals(args[0])) {
+                System.out.print(getOSName());
+                return;
+            }
+            else if ("--arch".equals(args[0])) {
+                System.out.print(getArchName());
+                return;
+            }
+        }
+
         System.out.print(getNativeLibFolderPathForCurrentOS());
     }
 
-    public static String getNativeLibFolderPathForCurrentOS()
-    {
+    public static String getNativeLibFolderPathForCurrentOS() {
         return getOSName() + "/" + getArchName();
     }
 
-    public static String getOSName()
-    {
+    public static String getOSName() {
         return translateOSNameToFolderName(System.getProperty("os.name"));
     }
 
-    public static String getArchName()
-    {
+    public static String getArchName() {
         return translateArchNameToFolderName(System.getProperty("os.arch"));
     }
 
-    public static String translateOSNameToFolderName(String osName)
-    {
-        if (osName.contains("Windows"))
-        {
+    public static String translateOSNameToFolderName(String osName) {
+        if (osName.contains("Windows")) {
             return "Windows";
         }
-        else if (osName.contains("Mac"))
-        {
+        else if (osName.contains("Mac")) {
             return "Mac";
         }
-        else if (osName.contains("Linux"))
-        {
+        else if (osName.contains("Linux")) {
             return "Linux";
         }
-        else
-        {
+        else {
             return osName.replaceAll("\\W", "");
         }
     }
 
-    public static String translateArchNameToFolderName(String archName)
-    {
+    public static String translateArchNameToFolderName(String archName) {
         return archName.replaceAll("\\W", "");
     }
 }
--- src/test/java/org/sqlite/OSInfoTest.java
+++ src/test/java/org/sqlite/OSInfoTest.java
@@ -9,16 +9,17 @@
 //--------------------------------------
 package org.sqlite;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.Test;
-import org.sqlite.OSInfo;
 
 public class OSInfoTest
 {
     @Test
-    public void osName()
-    {
+    public void osName() {
         assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows XP"));
         assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows 2000"));
         assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows Vista"));
@@ -37,8 +38,7 @@ public class OSInfoTest
     }
 
     @Test
-    public void archName()
-    {
+    public void archName() {
         assertEquals("i386", OSInfo.translateArchNameToFolderName("i386"));
         assertEquals("x86", OSInfo.translateArchNameToFolderName("x86"));
         assertEquals("ppc", OSInfo.translateArchNameToFolderName("ppc"));
@@ -46,12 +46,50 @@ public class OSInfoTest
     }
 
     @Test
-    public void folderPath()
-    {
+    public void folderPath() {
         String[] component = OSInfo.getNativeLibFolderPathForCurrentOS().split("/");
         assertEquals(2, component.length);
         assertEquals(OSInfo.getOSName(), component[0]);
         assertEquals(OSInfo.getArchName(), component[1]);
+    }
+
+    @Test
+    public void testMainForOSName() throws Exception {
+
+        // preserve the current System.out
+        PrintStream out = System.out;
+        try {
+            // switch STDOUT
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            PrintStream tmpOut = new PrintStream(buf);
+            System.setOut(tmpOut);
+            OSInfo.main(new String[] { "--os" });
+            assertEquals(OSInfo.getOSName(), buf.toString());
+        }
+        finally {
+            // reset STDOUT
+            System.setOut(out);
+        }
+
+    }
+
+    @Test
+    public void testMainForArchName() throws Exception {
+
+        // preserver the current System.out
+        PrintStream out = System.out;
+        try {
+            // switch STDOUT
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            PrintStream tmpOut = new PrintStream(buf);
+            System.setOut(tmpOut);
+            OSInfo.main(new String[] { "--arch" });
+            assertEquals(OSInfo.getArchName(), buf.toString());
+        }
+        finally {
+            // reset STDOUT
+            System.setOut(out);
+        }
     }
 
 }
