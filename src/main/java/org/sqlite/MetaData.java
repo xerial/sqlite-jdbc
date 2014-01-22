f22a1df7bd53dce960a870d5b68367065ccd220b
Grace Simon Batumbya <g.batumbya@outlook.com>
2014-01-21 19:23:35 -0500

    Fixes issue #107 DatabaseMetaData.getPrimaryKeys does not always return all primary keys for multi-line table definitions.

--- src/main/java/org/sqlite/MetaData.java
+++ src/main/java/org/sqlite/MetaData.java
@@ -1428,7 +1428,8 @@ class MetaData implements DatabaseMetaData
      * Pattern used to extract a named primary key.
      */
      protected final static Pattern FK_NAMED_PATTERN =
-         Pattern.compile(".* constraint +(.*?) +foreign +key *\\((.*?)\\).*", Pattern.CASE_INSENSITIVE);
+         Pattern.compile(".*\\sCONSTRAINT\\s+(.*?)\\s*FOREIGN\\s+KEY\\s*\\((.*?)\\).*",
+             Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
      /**
      * @see java.sql.DatabaseMetaData#getExportedKeys(java.lang.String, java.lang.String,
@@ -1911,13 +1912,15 @@ class MetaData implements DatabaseMetaData
      * Pattern used to extract column order for an unnamed primary key.
      */
     protected final static Pattern PK_UNNAMED_PATTERN =
-            Pattern.compile(".* primary +key *\\((.*?,+.*?)\\).*", Pattern.CASE_INSENSITIVE);
+        Pattern.compile(".*\\sPRIMARY\\s+KEY\\s+\\((.*?,+.*?)\\).*",
+            Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
     /**
      * Pattern used to extract a named primary key.
      */
      protected final static Pattern PK_NAMED_PATTERN =
-         Pattern.compile(".* constraint +(.*?) +primary +key *\\((.*?)\\).*", Pattern.CASE_INSENSITIVE);
+         Pattern.compile(".*\\sCONSTRAINT\\s+(.*?)\\s+PRIMARY\\s+KEY\\s+\\((.*?)\\).*",
+             Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
     /**
      * Parses the sqlite_master table for a table's primary key
