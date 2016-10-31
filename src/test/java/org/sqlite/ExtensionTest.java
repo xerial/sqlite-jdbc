8df0b6ccee1a58958cb3b8724a25a0640b2c547b
Taro L. Saito <leo@xerial.org>
2016-10-31 10:55:04 -0700

    #177: Add FTS5 test

--- src/test/java/org/sqlite/ExtensionTest.java
+++ src/test/java/org/sqlite/ExtensionTest.java
@@ -1,21 +1,20 @@
 package org.sqlite;
 
-import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
-public class ExtensionTest
-{
+public class ExtensionTest {
     Connection conn;
-    Statement  stat;
+    Statement stat;
 
     @Before
     public void setUp() throws Exception {
@@ -25,31 +24,39 @@ public class ExtensionTest
 
     @After
     public void tearDown() throws Exception {
-        if (stat != null)
+        if(stat != null)
             stat.close();
-        if (conn != null)
+        if(conn != null)
             conn.close();
 
     }
 
     @Test
     public void extFTS3() throws Exception {
-
         stat.execute("create virtual table recipe using fts3(name, ingredients)");
-        stat
-                .execute("insert into recipe (name, ingredients) values('broccoli stew', 'broccoli peppers cheese tomatoes')");
+        stat.execute("insert into recipe (name, ingredients) values('broccoli stew', 'broccoli peppers cheese tomatoes')");
         stat.execute("insert into recipe (name, ingredients) values('pumpkin stew', 'pumpkin onions garlic celery')");
 
         ResultSet rs = stat
-                .executeQuery("select rowid, name, ingredients from recipe where ingredients match 'onions'");
+            .executeQuery("select rowid, name, ingredients from recipe where ingredients match 'onions'");
         assertTrue(rs.next());
         assertEquals("pumpkin stew", rs.getString(2));
+    }
 
+    @Test
+    public void extFTS5() throws Exception {
+        stat.execute("create virtual table recipe using fts5(name, ingredients)");
+        stat.execute("insert into recipe (name, ingredients) values('broccoli stew', 'broccoli peppers cheese tomatoes')");
+        stat.execute("insert into recipe (name, ingredients) values('pumpkin stew', 'pumpkin onions garlic celery')");
+
+        ResultSet rs = stat
+            .executeQuery("select rowid, name, ingredients from recipe where recipe match 'onions'");
+        assertTrue(rs.next());
+        assertEquals("pumpkin stew", rs.getString(2));
     }
 
     @Test
     public void extFunctions() throws Exception {
-
         {
             ResultSet rs = stat.executeQuery("select cos(radians(45))");
             assertTrue(rs.next());
@@ -63,7 +70,6 @@ public class ExtensionTest
             assertEquals("TGCA", rs.getString(1));
             rs.close();
         }
-
     }
 
 
