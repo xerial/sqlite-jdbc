38fc7a70b175fb812ffd072e2588ef3c7e933510
martin.haeusler <martin.haeusler@txture.io>
2019-06-18 13:42:23 +0200

    Fixed the mis-spelled transaction mode enum crashing the JdbcHammer test

--- src/main/java/org/sqlite/jdbc3/JDBC3Connection.java
+++ src/main/java/org/sqlite/jdbc3/JDBC3Connection.java
@@ -31,7 +31,7 @@ public abstract class JDBC3Connection extends SQLiteConnection {
 
     }
 
-
+    @SuppressWarnings("deprecation")
     public void checkTransactionMode() throws SQLException {
         // important note: read-only mode is only supported when auto-commit is disabled
         if(this.getDatabase().getConfig().isExplicitReadOnlyEnabled() && !this.getAutoCommit() && this.getCurrentTransactionType() != null){
@@ -40,7 +40,7 @@ public abstract class JDBC3Connection extends SQLiteConnection {
                 // (note: this pragma is evaluated on a per-transaction basis by SQLite)
                 this.getDatabase()._exec("PRAGMA query_only = true;");
             }else{
-                if(this.getCurrentTransactionType() == TransactionMode.DEFFERED){
+                if(this.getCurrentTransactionType() == TransactionMode.DEFERRED || this.getCurrentTransactionType() == TransactionMode.DEFFERED){
                     if(this.isFirstStatementWasExecuted()){
                         // first statement was already executed; cannot upgrade to write transaction!
                         throw new SQLException("A statement has already been executed on this connection; cannot upgrade to write transaction!");
