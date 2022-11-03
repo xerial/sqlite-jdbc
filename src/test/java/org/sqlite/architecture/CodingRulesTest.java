9a19b8a08b01d1a40166eb5eef1149555ecc5851
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2022-11-03 17:08:22 +0800

    test: add architecture tests

---
+++ src/test/java/org/sqlite/architecture/CodingRulesTest.java
@@ -0,0 +1,31 @@
+package org.sqlite.architecture;
+
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.GeneralCodingRules.*;
+
+@AnalyzeClasses(packages = "org.sqlite", importOptions = {ImportOption.DoNotIncludeTests.class})
+class CodingRulesTest {
+
+//    Disabled for now, see https://github.com/xerial/sqlite-jdbc/issues/802
+//    @ArchTest
+    private final ArchRule no_access_to_standard_streams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+
+//    Disabled for now
+//    @ArchTest
+    private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+
+    @ArchTest
+    private final ArchRule no_jodatime = NO_CLASSES_SHOULD_USE_JODATIME;
+
+    @ArchTest
+    private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+
+    @ArchTest
+    public static final ArchRule no_junit_assertions = noClasses().should().dependOnClassesThat().haveFullyQualifiedName("org.junit.jupiter.api.Assertions");
+}
