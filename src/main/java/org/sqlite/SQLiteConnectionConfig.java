c06b022a299130c6a95aeecc68e543e2f076e36e
exoego <ytatsuno.jp@gmail.com>
2023-11-29 06:16:08 +0900

    format

--- src/main/java/org/sqlite/SQLiteConnectionConfig.java
+++ src/main/java/org/sqlite/SQLiteConnectionConfig.java
@@ -38,8 +38,8 @@ public class SQLiteConnectionConfig implements Cloneable {
                         pragmaTable.getProperty(
                                 SQLiteConfig.Pragma.TRANSACTION_MODE.pragmaName,
                                 SQLiteConfig.TransactionMode.DEFERRED.name())),
-                pragmaTable.getProperty(
-                        SQLiteConfig.Pragma.JDBC_AUTO_COMMIT.pragmaName, "true")
+                pragmaTable
+                        .getProperty(SQLiteConfig.Pragma.JDBC_AUTO_COMMIT.pragmaName, "true")
                         .equals("true"));
     }
 
