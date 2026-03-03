68c1b968acb2da140d840b6fa72ecfbff557db8f
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-03-03 12:00:53 +0800

    ci: speed up armv7 tests

--- src/test/java/org/sqlite/architecture/TestCodingRulesTest.java
+++ src/test/java/org/sqlite/architecture/TestCodingRulesTest.java
@@ -6,7 +6,14 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
 
+@DisabledIfEnvironmentVariable(
+        named = "SKIP_TEST_MULTIARCH",
+        matches = "true",
+        disabledReason = "Those tests don't need to run in multi-arch")
+@DisabledInNativeImage
 @AnalyzeClasses(
         packages = "org.sqlite",
         importOptions = {ImportOption.OnlyIncludeTests.class})
